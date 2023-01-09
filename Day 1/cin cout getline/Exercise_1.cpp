c56633bb3089e019ed8bfc8e9ef2a865f731b907
hoandoan <doanhanhoan03@berkeley.edu>
2023-01-08 22:06:44 -0800

    123

--- Day 1/cin cout getline/Exercise_1.cpp
+++ Day 1/cin cout getline/Exercise_1.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-
+int x;
+int y;
+int z;
+cout << "Enter the length, width, and height:";
+cin >> x;
+cin >> y;
+cin >> z;
+cout << "Volume:" << x*y*z << endl;
+cout << "Surface Area:" << 2*x*y + 2*x*z + 2*y*z << endl;
 
 }
--- Day 1/cin cout getline/Exercise_2.cpp
+++ Day 1/cin cout getline/Exercise_2.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 
 int main() {
+string name;
+string hobby;
+string phone;
+string first;
+string last;
+cout << "Enter Your Information" << endl <<
+"----------------------" << endl <<
+"First and Last Name: ";
+cin >> first >> last;
+cout << "Hobbies: ";
+cin.ignore();
+getline(cin, hobby);
+cout << "Phone Number: ";
+cin >> phone;
+cout <<"Here is your information" << endl <<
+"------------------------" << endl <<
+"First Name: " << first << endl <<
+"Last Name: " << last << endl <<
+"Your hobby: " << hobby << endl <<
+"Your phone number: " <<phone;
+
+
+
 
 
 }
